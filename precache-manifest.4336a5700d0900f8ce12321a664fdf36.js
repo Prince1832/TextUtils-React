9033a788f6efcbc1b34a7e3d8c92ecc710d8ba7a
Prince1832 <123098996+Prince1832@users.noreply.github.com>
2023-04-11 15:05:45 +0530

    Updates

---
+++ precache-manifest.4336a5700d0900f8ce12321a664fdf36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2d0df8b2ba273b1b774",
+    "url": "/TextUtils-React/static/css/main.6cd33f49.chunk.css"
+  },
+  {
+    "revision": "f2d0df8b2ba273b1b774",
+    "url": "/TextUtils-React/static/js/main.f2d0df8b.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/TextUtils-React/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/TextUtils-React/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "2591e466b5f270c0c477",
+    "url": "/TextUtils-React/static/js/runtime~main.2591e466.js"
+  },
+  {
+    "revision": "bba0605dedf6be50609eb28fbda0b48e",
+    "url": "/TextUtils-React/index.html"
+  }
+];
